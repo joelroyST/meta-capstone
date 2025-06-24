2dcf1a5ae751f6aa75d219ca767f52bbfe3b7ed0
Joel Joseph Roy <joelroy@meta.com>
2025-06-24 01:51:10 -0700

    frontend and backend initialization

---
+++ frontend/src/Components.jsx/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="top-section">
+                <div className="hamburg"></div>
+                <div className="user-profile-icon" src=""></div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
