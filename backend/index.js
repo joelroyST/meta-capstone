7e52a119fad4eb93e9cdce5916973c42d3b04876
Joel Joseph Roy <joelroy@meta.com>
2025-06-27 17:31:54 -0700

    Starts express backend and connects the routes for authentication

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const chalk = require('chalk')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
 
-server.use(cors())
-server.use(express.json())
-server.use(morgan('dev'))
+server.use(cors());
+server.use(express.json());
+server.use(morgan("dev"));
 
-const authRouter = require('./routes/auth')
-server.use("/auth", authRouter)
+const authRouter = require("./routes/auth");
+server.use("/auth", authRouter);
+const userRouter = require("./routes/user");
+server.use("/user", userRouter);
+
 const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
